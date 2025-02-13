165f43e126ce6adce6d367d777523235ca002977
Zak Kastl <zkastl8@gmail.com>
2025-02-13 10:35:57 -0600

    added rest of movement code; added citation for original spacesettlers by mcgovern

--- spacesettlers/utilities/movement.py
+++ spacesettlers/utilities/movement.py
@@ -16,9 +16,19 @@ class Movement:
 
     def __init__(self):
         self.translational_acceleration = Vector2D()
+        self.angular_acceleration = 0.0
 
     def set_translational_acceleration(self, translationalAcceleration: Vector2D) -> None:
         self.translational_acceleration = translationalAcceleration
 
-        if (translationalAcceleration.magnitude / Movement.MAX_TRANSLATIONAL_ACCELERATION):
-            pass
+        if translationalAcceleration.magnitude / Movement.MAX_TRANSLATIONAL_ACCELERATION:
+            ratio = translationalAcceleration.get_magnitude() / Movement.MAX_TRANSLATIONAL_ACCELERATION
+            self.translational_acceleration = self.translational_acceleration.multiply(1. / ratio)
+
+    def set_angular_accleration(self, angularAccleration) -> None:
+        if angularAccleration > Movement.MAX_ANGLULAR_ACCELERATION:
+            self.angular_acceleration = Movement.MAX_ANGLULAR_ACCELERATION
+        elif angularAccleration < -Movement.MAX_ANGLULAR_ACCELERATION:
+            self.angular_acceleration = -Movement.MAX_ANGLULAR_ACCELERATION
+        else:
+            self.angular_acceleration = angularAccleration
